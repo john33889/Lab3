e724a90407c74c8c848827372c5b5ee50dc13f7b
john33889 <105161975+john33889@users.noreply.github.com>
2023-10-19 13:01:28 +0300

    Update gps.js

--- gps.js
+++ gps.js
@@ -14,7 +14,7 @@ var latlon=position.coords.latitude +"," +position.coords.longitude;
 var img_url=
 "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+
 
-"&zoom=14&size=400x300&key=YOUR_ KEY";
+"&zoom=14&size=400x300&key=AIzaSyBZGvjtL3W4rA8cWoCluwG1hb-TjeSHsog";
 
 document.getElementById("map").innerHTML="<img src='"+img_url+"'>";
 }
