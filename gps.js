090fe1364da7613bb2bf7f02dabf7b5f46fe851c
john33889 <105161975+john33889@users.noreply.github.com>
2023-10-19 13:16:20 +0300

    Update gps.js

--- gps.js
+++ gps.js
@@ -1,11 +1,11 @@
 get_geo_position();
 function on_success (position){
 
-document.getElementById("latitude") .innerHTML="Latitude ="+ position.coords.latitude;
+document.getElementById("latitude").innerHTML="Latitude ="+ position.coords.latitude;
 document.getElementById("longitude").innerHTML="Longitude= " + position.coords.longitude;
-document.getElementById("acc") .innerHTML="Accuracy ="+position.coords.accuracy;
+document.getElementById("acc").innerHTML="Accuracy ="+position.coords.accuracy;
 document.getElementById("altitude").innerHTML="Altitude ="+position.coords.altitude;
-var latlon=position.coords.latitude +"," +position.coords.longitude;
+var latlon=position.coords.latitude +","+position.coords.longitude;
 
 var img_url="https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyBZGvjtL3W4rA8cWoCluwG1hb-TjeSHsog";
 
